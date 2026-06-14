41a54d500cad767076397ad007e49e7e13adbc0e
franklindot04 <a2andwinning@gmail.com>
2026-06-14 03:23:15 +0300

    examples: add java basics demos

---
+++ examples/beginner/LoopsExample.java
@@ -0,0 +1,13 @@
+public class LoopsExample {
+    public static void main(String[] args) {
+        for (int number = 1; number <= 5; number++) {
+            System.out.println("For loop number: " + number);
+        }
+
+        int countdown = 3;
+        while (countdown > 0) {
+            System.out.println("Countdown: " + countdown);
+            countdown--;
+        }
+    }
+}
